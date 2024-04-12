55fb47eabe102b7bd5ee608b93085ea46b4da7ad
Bhagyashree-rai <68314938+Bhagyashree-rai@users.noreply.github.com>
2024-04-12 13:35:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function downloadCV(){
+        // // Create a link element
+        // var downloadLink = document.createElement('a');
+        // // Set the href attribute to the path of your resume file
+        // downloadLink.href = './assets/Bhagyashree_Resume(1).pdf';
+        // // Set the download attribute to specify the filename to be downloaded
+        // downloadLink.download = 'Bhagyashree_Resume.pdf';
+        // // Append the link to the body
+        // document.body.appendChild(downloadLink);
+        // // Programmatically trigger the click event of the link
+        // downloadLink.click();
+        // // Remove the link from the body
+        // document.body.removeChild(downloadLink);
+        document.getElementById('downloadLink').click();
+;}
